ca6f970c3cd496c251e83bbb78bed3f3ecefa4a8
vargen2 <hjaarn2@gmail.com>
2019-02-17 17:36:18 +0100

    css and js sorting

--- ServerStats/src/main/java/se/iths/mhb/plugin/ServerStats.java
+++ ServerStats/src/main/java/se/iths/mhb/plugin/ServerStats.java
@@ -14,26 +14,34 @@ public class ServerStats implements HttpService {
     @RequestMethod
     public HttpResponse getStatsHtmlPage(HttpRequest httpRequest) {
         StringBuilder stats = new StringBuilder();
-        pageHits.forEach((s, integer) -> stats.append("<tr>\n<td>" + s + "</td>\n<td>" + integer + "</td>\n</tr>\n"));
+        pageHits.forEach((s, integer) -> stats.append(
+                "       <tr class=\"item\">\n" +
+                        "<td>" + s + "</td>\n" +
+                        "<td>" + integer + "</td>\n" +
+                        "</tr>\n"));
 
 
         String page = "<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>" +
                 "    <link rel=\"stylesheet\" href=\"/w3.css\">" +
+                "    <link rel=\"stylesheet\" href=\"w3-colors-flat.css\">" +
+                "<script src=\"w3.js\"></script>" +
                 "    <title>Server Stats</title>\n" +
                 "</head>\n" +
-                "<body>\n" +
+                "<body style=\"color:#141414; background-color:snow; max-width:750px\">\n" +
+                "<div class=\"w3-container\">" +
                 "<H3>Server Stats</H3>\n" +
                 "\n" +
 
-                "<table class=\"w3-table w3-border w3-striped w3-hoverable\">\n" +
+                "<table class=\"w3-table w3-border w3-striped w3-hoverable\" id=\"statstable\">\n" +
                 "<thead class=\"w3-flat-green-sea\">\n" +
                 "<tr>\n" +
-                "<th>Page</th>\n" +
-                "<th>Hits</th>\n" +
-                "</tr>\n" +
+                "<th onclick=\"w3.sortHTML('#statstable','.item', 'td:nth-child(1)')\" style=\"cursor:pointer\">Page</th>\n" +
+                "<th onclick=\"w3.sortHTML('#statstable','.item', 'td:nth-child(2)')\" style=\"cursor:pointer\">Hits</th>\n" +
+                "</tr >\n" +
                 "</thead>\n" +
                 "<tbody>\n" +
 
@@ -41,7 +49,7 @@ public class ServerStats implements HttpService {
 
                 "</tbody>\n" +
                 "</table>\n" +
-
+                "</div>" +
 
                 "</body>\n" +
                 "</html>";
